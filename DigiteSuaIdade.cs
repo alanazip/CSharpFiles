a17f0fb8d7e16a2862e6f9a1cd2fd5dcc5607c98
Alana Souza <alananascimentodesouza@gmail.com>
2021-10-28 20:31:58 -0300

    zoeira

---
+++ DigiteSuaIdade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigiteSuaIdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void B_Idade_Click(object sender, EventArgs e)
+        {
+            String idade;
+            idade = TB_Idade.Text;
+            MessageBox.Show("Sua idade é " + idade + " anos.");
+        }
+    }
+}
